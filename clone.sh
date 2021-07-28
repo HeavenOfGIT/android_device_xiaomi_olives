3ff90722f957c04563ec77ac06b477b027393ec8
skittels15 <50242333+skittels15@users.noreply.github.com>
2021-07-28 20:54:16 +0300

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -3,15 +3,15 @@ cd ../../../
 echo "Olives Trees Cloning Script"
 echo ""
 echo "Clone device tree..."
-git clone https://github.com/olives-device/android_device_xiaomi_msm8937-common device/xiaomi/msm8937-common
-git clone https://github.com/olives-device/android_device_xiaomi_sdm439-common device/xiaomi/sdm439-common
+git clone https://github.com/Xtended-Devices/android_device_xiaomi_msm8937-common device/xiaomi/msm8937-common
+git clone https://github.com/Xtended-Devices/android_device_xiaomi_sdm439-common device/xiaomi/sdm439-common
 echo "Clone kernel tree..."
-git clone https://github.com/olives-device/android_kernel_xiaomi_sdm439 kernel/xiaomi/sdm439
+git clone https://github.com/Xtended-Devices/android_kernel_xiaomi_sdm439 kernel/xiaomi/sdm439
 echo "Clone vendor trees..."
-git clone https://github.com/olives-device/proprietary_vendor_xiaomi_msm8937-common vendor/xiaomi/msm8937-common
-git clone https://github.com/olives-device/proprietary_vendor_xiaomi_sdm439-common vendor/xiaomi/sdm439-common
-git clone https://github.com/olives-device/proprietary_vendor_xiaomi_olives vendor/xiaomi/olives
-git clone https://github.com/olives-device/proprietary_vendor_xiaomi_olives-camera vendor/xiaomi/olives-camera
+git clone https://github.com/Xtended-Devices/proprietary_vendor_xiaomi_msm8937-common vendor/xiaomi/msm8937-common
+git clone https://github.com/Xtended-Devices/proprietary_vendor_xiaomi_sdm439-common vendor/xiaomi/sdm439-common
+git clone https://github.com/Xtended-Devices/proprietary_vendor_xiaomi_olives vendor/xiaomi/olives
+git clone https://github.com/Xtended-Devices/proprietary_vendor_xiaomi_olives-camera vendor/xiaomi/olives-camera
 echo ""
 echo "File cloning completed!"
 # Clone end
